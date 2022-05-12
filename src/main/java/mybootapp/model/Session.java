299777aeb1d15bf4c112b21925100001b3f4976e
Aoryu <damien.coq2311@gmail.com>
2022-05-12 19:04:51 +0200

    completion Session sans contraintes

--- src/main/java/mybootapp/model/Session.java
+++ src/main/java/mybootapp/model/Session.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
 
 @Entity
 @Data
@@ -18,4 +19,39 @@ public class Session implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int idSession;
 
+    @Basic()
+    @Column(name = "codeSession")
+    private int codeSession;
+
+    @Basic()
+    @Temporal(TemporalType.DATE)
+    @Column(name = "dateMaj")
+    private Date dateMaj;
+
+    @Basic()
+    @Temporal(TemporalType.DATE)
+    @Column(name = "dateCrea")
+    private Date dateCrea;
+
+    @Basic()
+    @Temporal(TemporalType.DATE)
+    @Column(name = "debut")
+    private Date debut;
+
+    @Basic()
+    @Temporal(TemporalType.DATE)
+    @Column(name = "fin")
+    private Date fin;
+
+    @Basic()
+    @Column(name = "sessionOuverte")
+    private boolean sessionOuverte;
+
+    @Basic()
+    @Column(name = "etatRecrutement")
+    private int etatRecrutement;
+
+    @Basic()
+    @Column(name = "extraInfoGarantie")
+    private boolean extraInfoGarantie;
 }
