b14755666e384e85cad66a4613f00c47cab692db
Daryun <42786244+aoryuh@users.noreply.github.com>
2022-05-16 21:26:50 +0200

    Delete AdresseRepo.java

--- src/main/java/mybootapp/repo/AdresseRepo.java
+++
@@ -1,7 +0,0 @@
-package mybootapp.repo;
-
-import mybootapp.model.Adresse;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdresseRepo extends JpaRepository<Adresse, Long> {
-}
