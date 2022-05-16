4f434095117da6b2be47b9e4e3f56590ee9ec929
Daryun <42786244+aoryuh@users.noreply.github.com>
2022-05-16 21:27:10 +0200

    Delete SessionRepo.java

--- src/main/java/mybootapp/repo/SessionRepo.java
+++
@@ -1,7 +0,0 @@
-package mybootapp.repo;
-
-import mybootapp.model.Session;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SessionRepo extends JpaRepository<Session, Long> {
-}
