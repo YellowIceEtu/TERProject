006b34f2ae39eb778f8045941eefd86a3d0bef0e
Daryun <42786244+aoryuh@users.noreply.github.com>
2022-05-16 21:27:17 +0200

    Delete UtilisateurRepo.java

--- src/main/java/mybootapp/repo/UtilisateurRepo.java
+++
@@ -1,7 +0,0 @@
-package mybootapp.repo;
-
-import mybootapp.model.Utilisateur;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UtilisateurRepo extends JpaRepository<Utilisateur, Long> {
-}
