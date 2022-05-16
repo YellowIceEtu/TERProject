2c9e2a515467dcbcaba31ba92d8379440ef1af2c
Daryun <42786244+aoryuh@users.noreply.github.com>
2022-05-16 21:27:04 +0200

    Delete CoordonneeRepo.java

--- src/main/java/mybootapp/repo/CoordonneeRepo.java
+++
@@ -1,7 +0,0 @@
-package mybootapp.repo;
-
-import mybootapp.model.Coordonnee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CoordonneeRepo extends JpaRepository<Coordonnee, Long> {
-}
