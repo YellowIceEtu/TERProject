0e8d3cf87dfe35c832171e5013692d7ce5003384
h17030347 <hal.kati@yahoo.fr>
2022-05-30 16:46:02 +0200

    Le bouton attribuer un nouveau role marche !!

--- src/main/java/mybootapp/controller/UtilisateurController.java
+++ src/main/java/mybootapp/controller/UtilisateurController.java
@@ -1,26 +1,18 @@
 package mybootapp.controller;
 
-
-import mybootapp.model.Adresse;
 import mybootapp.model.Composante;
-import mybootapp.model.Formation;
 import mybootapp.model.Utilisateur;
 import mybootapp.repo.ComposanteRepo;
 import mybootapp.repo.UtilisateurRepo;
 import mybootapp.service.PermissionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.Collection;
+
 
 @RequestMapping("/admin")
 @Controller
@@ -81,25 +73,6 @@ public class UtilisateurController {
 
         return new ModelAndView("listUtilisateur", "utilisateurs", utilisateurRepo.findAll());
     }
-
-/*
-    @RequestMapping(value = "/editRole/{id}")
-    public ModelAndView editRole(@PathVariable Long id) {
-
-
-        if(utilisateurRepo.findById(id).isPresent()){
-            Utilisateur utilisateur = utilisateurRepo.findById(id).get();
-            if(utilisateur.isEstAdmin()) {
-            permissionManager.removeAdmin(utilisateur);
-            utilisateurRepo.save(utilisateur);
-            }
-            permissionManager.AddAdmin(utilisateur);
-            utilisateurRepo.save(utilisateur);
-        }
-
-        return new ModelAndView("listUtilisateur", "utilisateurs", utilisateurRepo.findAll());
-    }
-*/
 
     @RequestMapping(value = "/editRoleUser/{id}")
     public ModelAndView editRole(@PathVariable Long id) {
