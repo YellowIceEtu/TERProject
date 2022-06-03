24d947283eb4b551df486a42e3b40105ca6b7e51
Aoryu <damien.coq2311@gmail.com>
2022-06-03 17:50:36 +0200

    refactor

--- src/main/java/mybootapp/controller/FormationController.java
+++ src/main/java/mybootapp/controller/FormationController.java
@@ -158,7 +158,7 @@ public class FormationController {
         return listBuilder.preRequisList();
     }
 
-
+    @ModelAttribute("formation")
     @RequestMapping(value = "formationDetails/edit", method = RequestMethod.POST)
     public String saveFormation(@ModelAttribute("formation") @Valid Formation formation, BindingResult result) {
         if (result.hasErrors()) {
